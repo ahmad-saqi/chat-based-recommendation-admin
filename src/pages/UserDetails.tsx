344b4eae39a303b1fb7856f692353bc0429227c3
saqiahmad1122 <ahmad_saqi@digimarkdevelopers.com>
2024-09-13 17:31:47 +0500

    show user from jason in user detail

--- src/pages/UserDetails.tsx
+++ src/pages/UserDetails.tsx
@@ -1,8 +1,17 @@
 import GlobalLayout from "../components/GlobalLayout";
 import profileimg from "../assets/img/image.svg";
 import UsageGraph from "../components/UsageGraph";
+import { users } from "../lib/data/userList";
+import { useParams } from "react-router-dom";
 
 const UserDetails = () => {
+   const { id } = useParams<{ id: string }>();
+   const userId = parseInt(id ?? "0", 10); // Provide a fallback if `id` is undefined
+   const user = users.find((user) => user.id === userId);
+
+   if (!user) {
+     return <p>User not found</p>;
+   }
   return (
     <GlobalLayout>
       <div className="w-full h-full flex flex-col items-start justify-start px-5 pb-8 bg-white">
@@ -10,7 +19,7 @@ const UserDetails = () => {
           <div className="w-full h-20 rounded-t-xl bg-gradient-to-t from-primary/40 via-primary/30 to-primary/15 border-b-primary/50 border-b-2"></div>
           <div className="w-full pt-10 px-5 pb-2.5 h-40 rounded-b-xl bg-white flex flex-col items-center justify-center shadow-down ">
             <p className="w-full text-left text-xl font-semiboldbold mt-10 md:mt-0">
-              Alec Thompson
+              {user.name}
             </p>
             <div className="w-full flex items-center justify-between ">
               <p className="text-sm text-gray-500">
@@ -29,7 +38,7 @@ const UserDetails = () => {
               </div>
             </div>
             <img
-              src={profileimg}
+              src={user.image}
               alt="profile"
               className="size-20 rounded-full absolute top-10 left-5 "
             />
@@ -37,7 +46,7 @@ const UserDetails = () => {
         </div>
       </div>
 
-     
+      {/* Reduce gap between sections */}
       <div className="w-full px-5 bg-white">
         <UsageGraph />
       </div>
